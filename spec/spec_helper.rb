1891cf2bb039489035728fd06acc6db72c642e4f
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-02-22 00:26:34 -0800

    Clean up specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,8 @@
-# require 'rspec'
-# require 'entangled'
-
-# ENV["RAILS_ENV"] ||= 'test'
-# require File.expand_path("../dummy/config/environment", __FILE__)
-
-# RSpec.configure do |config|
-#   config.include RSpec::Rails::RequestExampleGroup, type: :routing
-# end
-
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
-require 'byebug'
-# require 'rspec/autorun'
 # require 'factory_girl_rails'
-
-Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
-end
