e65e89a29a42eb013c446b40944d1663803aae91
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-02-25 17:55:41 -0800

    Add example test to Angular app

--- spec/dummy/public/js/app.js
+++ spec/dummy/public/js/app.js
@@ -68,18 +68,3 @@ angular.module('entangledTest', [
     });
   });
 })
-
-.service('FibonacciService', function() {
-  // iterative approach
-  this.fibonacci = function(num) {
-    var prev1 = 1,
-        prev2 = 0,
-        current = 0;
-    for (var n = 2; n <= num; n++) {
-      current = prev1 + prev2;
-      prev2 = prev1;
-      prev1 = current;
-    }
-    return current;
-  };
-})
