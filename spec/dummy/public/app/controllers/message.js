bea855185d342ce57227891c1fcb0169a682f5ad
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-02-26 20:50:53 -0800

    Break up dummy frontend into several files

---
+++ spec/dummy/public/app/controllers/message.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('entangledTest')
+
+.controller('MessageCtrl', function($scope, $routeParams, Message) {
+  $scope.update = function() {
+    $scope.message.$save();
+  };
+
+  Message.find($routeParams.id, function(message) {
+    $scope.$apply(function() {
+      $scope.message = message;
+      console.log('Show callback called!');
+    });
+  });
+});
