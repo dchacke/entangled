e1b0e440915ef733662864105f6fe0e26aaac01f
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-03-04 15:48:39 -0800

    Reduce service code

--- spec/dummy/public/app/services/message.js
+++ spec/dummy/public/app/services/message.js
@@ -3,22 +3,5 @@
 angular.module('entangledTest')
 
 .factory('Message', function(Entangled) {
-  var entangled = new Entangled('ws://localhost:3000/messages');
-
-  var Message = {
-    new: function(params) {
-      return entangled.new(params);
-    },
-    all: function(callback) {
-      return entangled.all(callback);
-    },
-    find: function(id, callback) {
-      return entangled.find(id, callback);
-    },
-    create: function(params, callback) {
-      return entangled.create(params, callback);
-    }
-  };
-
-  return Message;
+  return new Entangled('ws://localhost:3000/messages');
 });
