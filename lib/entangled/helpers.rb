e9f21a2d50e307de909d888f31a3ac790b186b8c
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-03-05 17:18:59 -0800

    Test helper's Redis behavior

--- lib/entangled/helpers.rb
+++ lib/entangled/helpers.rb
@@ -3,9 +3,9 @@ module Entangled
     # Get Redis that user might be using or instantiate
     # a new one
     def redis
-      if defined?($redis)
+      if defined?($redis) && $redis
         Redis.new($redis.client.options)
-      elsif defined?(REDIS)
+      elsif defined?(REDIS) && REDIS
         Redis.new(REDIS.client.options)
       else
         Redis.new
