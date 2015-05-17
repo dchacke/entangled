c0b30380b4c780ea1221afee6f07eedb3e61cc6f
Abdul Miah <momun20@gmail.com>
2015-05-17 18:05:15 -0500

    master always create new redis instance

--- lib/entangled/helpers.rb
+++ lib/entangled/helpers.rb
@@ -3,13 +3,7 @@ module Entangled
     # Get Redis that user might be using or instantiate
     # a new one
     def redis
-      if defined?($redis) && $redis
-        Redis.new($redis.client.options)
-      elsif defined?(REDIS) && REDIS
-        Redis.new(REDIS.client.options)
-      else
-        Redis.new
-      end
+      Redis.new
     end
   end
 end
